18a8d7507e371cbc1d5a739b892979bf858ea2c0
Quoc Tran <quoc.tran@hearti.io>
2020-11-21 13:25:41 +0700

    Fixed tailwind

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./styles/index.scss";
+
 function App() {
   return (
     <div className="bg-gray-600 h-screen flex justify-center items-center flex-col">
-      <img src="/assets/logo.svg" className="w-96" alt="logo" />
+      <img src="/assets/logo.svg" className="w-96 animate-spin-slow spin-slow" alt="logo" />
       <div className="text-white text-xl border-2 block p-2">COMING SOON</div>
     </div>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
